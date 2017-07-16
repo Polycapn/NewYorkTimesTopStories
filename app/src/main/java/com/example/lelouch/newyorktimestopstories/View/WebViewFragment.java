cbe9e098836f60d1d22c2e87681a482dd15b275a
Polycap <polycapn@gmail.com>
2017-07-16 16:54:39 -0500

    implement Webview Fragment and Webview binder

--- app/src/main/java/com/example/lelouch/newyorktimestopstories/View/WebViewFragment.java
+++ app/src/main/java/com/example/lelouch/newyorktimestopstories/View/WebViewFragment.java
@@ -1,6 +1,5 @@
 package com.example.lelouch.newyorktimestopstories.View;
 
-import android.app.ProgressDialog;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -8,9 +7,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
 import com.example.lelouch.newyorktimestopstories.R;
 import com.example.lelouch.newyorktimestopstories.databinding.WebviewBinding;
@@ -27,25 +23,9 @@ public class WebViewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         WebviewBinding binding = DataBindingUtil.inflate(inflater, R.layout.webview, container, false);
-        ProgressDialog progressDialog = ProgressDialog.show(getActivity(), "Loading article", "Please wait", true);
-        progressDialog.setCancelable(true);
-        progressDialog.show();
-
-
         Bundle arguments = getArguments();
         String webUrl = arguments.getString(WEB_URL);
-        WebView webView = binding.webView;
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                progressDialog.dismiss();
-            }
-        });
-        WebSettings settings = webView.getSettings();
-        settings.setJavaScriptEnabled(true);
-        settings.setLoadWithOverviewMode(true);
-        settings.setUseWideViewPort(true);
-        webView.loadUrl(webUrl);
+        binding.setWebUrl(webUrl);
         return binding.getRoot();
     }
 }
--- app/src/main/java/com/example/lelouch/newyorktimestopstories/util/WebviewBinder.java
+++ app/src/main/java/com/example/lelouch/newyorktimestopstories/util/WebviewBinder.java
@@ -1,8 +1,12 @@
 package com.example.lelouch.newyorktimestopstories.util;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.databinding.BindingAdapter;
 import android.text.TextUtils;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 /**
  * Created by lelouch on 7/15/2017.
@@ -13,7 +17,26 @@ public class WebviewBinder {
     @BindingAdapter("app:webUrl")
     public static void setWebView(WebView webView, String webUrl) {
         if (TextUtils.isEmpty(webUrl)) return;
+        ProgressDialog progressDialog = setUpProgressDialog(webView.getContext());
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                progressDialog.dismiss();
+            }
+        });
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setLoadWithOverviewMode(true);
+        settings.setUseWideViewPort(true);
         webView.loadUrl(webUrl);
+    }
+
+    private static ProgressDialog setUpProgressDialog(Context context) {
+        ProgressDialog progressDialog = ProgressDialog.show(context, "Loading article", "Please wait", true);
+        progressDialog.setCancelable(true);
+        progressDialog.show();
+        return progressDialog;
     }
 
 
